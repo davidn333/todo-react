52f53a25cc7fa6fc9f8c664d9b6f7f6f8f4b3682
David <david_2_9@hotmail.co.uk>
2020-02-25 06:48:37 +0000

    first commit

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './Task.css';
 
-function Task() {
-  return (
-    <div className="Task">
-      <p>Task</p>
-    </div>
-  );
+class Task extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  toggleDone(i) {
+    this.setState({done: !this.state.done});
+    console.log(this.state.done);
+  }
+
+  componentDidUpdate() {
+    if(this.state.done){
+      console.log(this.props.id);
+      document.getElementById(this.props.id).style.setProperty("text-decoration", "line-through");
+    }else{
+      document.getElementById(this.props.id).style.setProperty("text-decoration", "none");
+    }
+  }
+
+  render() {
+
+    
+
+    return (
+      
+        <span>
+          <p id={this.props.id} className="Task">{this.props.value}</p>
+          <button onClick={() => this.toggleDone(this.props.id)}>Done</button>
+        </span>
+    );
+  }
 }
 
 export default Task;
--- src/components/tasklist/TaskList.js
+++ src/components/tasklist/TaskList.js
@@ -1,14 +1,84 @@
 import React from 'react';
 import './TaskList.css';
+import Task from '../task/Task.js';
 
-function TaskList() {
-  return (
-    <div className="TaskList">
+class TaskList extends React.Component {
 
-      <input type='submit' value='Create Task' placeholder="What needs doing?"/>
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+    
+  }
 
-    </div>
-  );
+  allTasks = () => {
+
+    // for (var i = 0; i<localStorage.length; i++) {
+    //     this.state.tasks.push(localStorage.getItem(localStorage.key(i)));
+    // };
+    console.log("All tasks: " + this.state.tasks);
+  }
+
+  UNSAFE_componentWillMount() {
+    this.allTasks();
+  }
+
+  // We use an arrow function so that 'this' refers to the TaskList and not within the scope of the 'createTask' function. Another option is to 'bind' the function.
+  createTask = (e) => {
+
+    if (e.key === 'Enter') {
+      // let nextIndex = this.state.tasks.length;
+      // console.log("index for task: "+nextIndex);
+      // localStorage.setItem('task' + nextIndex, e.target.value);
+      // console.log("index for retrieval of task: "+nextIndex);
+      // console.log(localStorage.key(0));
+      // this.state.tasks.push(localStorage.getItem('task'+nextIndex));
+      console.log(this.state.tasks);
+      console.log("item added to array, new lenght: "+this.state.tasks.length);
+      // Calling setState will trigger componentDidUpdate and the render function. Or can use forceUpdate
+      
+      this.state.tasks.push(e.target.value);
+      this.setState({tasks: this.state.tasks});
+      // this.forceUpdate();
+      document.getElementById('input').value = "";
+    }
+  }
+
+  removeTask(i) {
+    console.log("Removed task, task index: "+i);
+    // localStorage.removeItem("task"+i);
+    this.state.tasks.splice(i,1);
+    this.setState({tasks: this.state.tasks});
+  }
+
+  render() {
+
+    let taskArray = this.state.tasks;
+    console.log("Array of tasks during render: "+taskArray);
+    return (
+      <div className="TaskList">
+
+        
+        <input id="input" placeholder="What needs doing?" onKeyDown={this.createTask}/>
+
+        <ul>
+
+          {taskArray.map((task, i) => {
+            return <li style={{color: "white"}} key={i}> 
+                      <span>
+                        <Task id={i} value={task} /> 
+                        
+                        <button onClick={() => this.removeTask(i)}>Remove</button>
+                      </span>
+                   </li>;
+          })}
+
+        </ul>
+
+      </div>
+    );
+  }
 }
 
 export default TaskList;
